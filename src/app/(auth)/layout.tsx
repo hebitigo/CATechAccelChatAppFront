3c8d5cf9784fd5357408179aa65816b607491fa4
hebitigo <60952925+hebitigo@users.noreply.github.com>
2024-02-04 14:42:29 +0900

    Revert "サイドバーの機能の追加"

--- src/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex flex-col justify-center items-center  h-screen bg-slate-800">
-      {children}
-    </div>
-  );
-}
